976047d49f6c07e3c3b364b74b4e90a27e12c900
Alexander Heimonen <alexander.heimonen@ciber.com>
2016-02-10 14:30:25 +0100

    Initial commit

---
+++ DesktopBackgroundChanger/DesktopBackgroundChanger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopBackgroundChanger
+{
+    class Program
+    {
+        private const string ImagesFolder = "Images";
+
+        static void Main(string[] args)
+        {
+            var fileName = GetRandomFileName(); 
+            WallpaperChanger.Set(fileName, WallpaperChanger.Style.Centered);
+        }
+
+        private static string GetRandomFileName()
+        {
+            var files = Directory.GetFiles(ImagesFolder);
+            var fileCount = files.Length;
+            var random = new Random();
+            var randomIndex = random.Next(0, fileCount - 1);
+            var randomFile = files.ElementAt(randomIndex);
+            var fileName = randomFile.Split('\\').Last();
+            return fileName;
+        }
+    }
+}
---
+++ DesktopBackgroundChanger/DesktopBackgroundChanger/WallpaperChanger.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace DesktopBackgroundChanger
+{
+    public class WallpaperChanger
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void Set(string fileName, Style style)
+        {
+            string filePath = Path.Combine("Images", fileName);
+            Image image = Image.FromFile(filePath, true);
+            string tempPath = Path.Combine(Path.GetTempPath(), fileName);
+            image.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+            
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+    }
+}
